3f52401948438d39be083a283c545719169fd11f
Shreya Sharma <shreyasharma3075@gmail.com>
2026-07-08 12:45:15 +0530

    Add GET tasks endpoint with in-memory data

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const tasks = require("./data/tasks");
 const app = express();
 app.get("/",(req,res)=>{
-    res.send("Task Manager is running ...");
+    res.send("Task Manager is working");
+})
+app.get("/tasks",(req,res)=>{
+    res.json(tasks);
 });
 app.listen(3000,()=>{
     console.log("Server is running...");
---
+++ data/tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Study Express",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "Build Task Manager API",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Practice JavaScript",
+    completed: true
+  }
+];
+
+module.exports = tasks;

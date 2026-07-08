c22a9967af3eb22853007ecd63981305fbbf9a9e
Shreya Sharma <shreyasharma3075@gmail.com>
2026-07-08 13:39:56 +0530

    Add POST task endpoint

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const tasks = require("./data/tasks");
 const app = express();
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Task Manager is working");
 })
 app.get("/tasks",(req,res)=>{
     res.json(tasks);
+});
+app.post("/tasks",(req,res)=>{
+    const {title} = req.body;
+    const newTask = {
+        id : tasks.length+1,
+        title : title,
+        completed : false
+    };
+    tasks.push(newTask);
+    res.json(newTask);
 });
 app.listen(3000,()=>{
     console.log("Server is running...");

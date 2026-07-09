9ac8b4530268ca89c3484e61f74a34d1edbe6454
Shreya Sharma <shreyasharma3075@gmail.com>
2026-07-09 23:02:36 +0530

    Implement task retrieval and update endpoints

--- app.js
+++ app.js
@@ -2,22 +2,67 @@ const express = require("express");
 const tasks = require("./data/tasks");
 const app = express();
 app.use(express.json());
+
+//home
+
 app.get("/",(req,res)=>{
     res.send("Task Manager is working");
 })
+
+//tasks GET
+
 app.get("/tasks",(req,res)=>{
     res.json(tasks);
 });
+
+//tasks POST
+
 app.post("/tasks",(req,res)=>{
     const {title} = req.body;
     const newTask = {
         id : tasks.length+1,
-        title : title,
+        title,
         completed : false
     };
     tasks.push(newTask);
-    res.json(newTask);
+    res.status(201).json(newTask);
 });
+
+//get tasks by id
+
+app.get("/tasks/:id",(req,res)=>{
+    const taskId = Number(req.params.id);
+    const task = tasks.find((task)=>{
+        return taskId === task.id;});
+        if(!task){
+            return res.status(404).json({
+                message : "Task not found.\n"
+            });
+        }
+    res.status(200).json(task);
+});
+
+//change to a task through id and  PUT
+
+app.put("/tasks/:id", (req,res)=>{
+    const taskId = Number(req.params.id);
+    const {title , completed} = req.body;
+
+    const task = tasks.find((task)=>{
+        return taskId === task.id;
+    });
+    if(!task){
+            return res.status(404).json({
+                message : "Task not found.\n"
+            });
+    }
+        task.title = title;
+        task.completed = completed;
+        
+    
+    res.status(200).json(task);
+    });
+
 app.listen(3000,()=>{
     console.log("Server is running...");
 });
